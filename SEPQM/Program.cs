c4cbf14db899c17960b0870d84e9e21f5ba4e8ab
Avdunusinghe <avdunusinghe@gmail.com>
2022-04-16 12:03:26 +0530

    Remove Comments

--- SEPQM/Program.cs
+++ SEPQM/Program.cs
@@ -19,61 +19,6 @@ namespace SEPQM
 
             internationalBranchASchoolObj.GetBranchMangerName();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            //Excecuting the constructors when object is intanciated
-            Laptop laptop = new Laptop(1, "Hp i7", "0002", 275000.00, "Hewlett-Packard Development Company, L.P.");
-
-            Laptop laptop1 = new Laptop();
-
-            int showRoomId = 1;
-
-            //First Step => Instansiate a Reference
-            Customer customer = new Customer();
-
-            //Second Step => Calling The Functions
-
-
-
-            laptop.PrintLaptopDetails(customer, showRoomId);
-            laptop.PrintLaptopDetails("Hp");
-            laptop.updateLaptopDetails();
         }
     }
 }
